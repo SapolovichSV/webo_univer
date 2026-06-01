0353f75522fd09dc2ce88b3b7502cd5903d8e367
StasSapi <ballplayerr44@gmail.com>
2026-06-01 18:00:00 +1000

    finish lab3

--- lab2django/admin.py
+++ lab2django/admin.py
@@ -1,13 +1,17 @@
+from django.utils.html import format_html
 from django.contrib import admin
 
-from .models import Cart,Product,ProductInCart,Customer
+from .models import Cart, Product, ProductInCart, Customer
+
+
 class ProductInCartInline(admin.TabularInline):
     model = ProductInCart
+
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     inlines = [ProductInCartInline]
-    list_display = ("id","customer","created_at")
+    list_display = ("id", "customer", "created_at", "total_price", "total_quantity","is_active")
     search_fields = (
         "customer__name",
         "customer__surname",
@@ -16,6 +20,20 @@ class CartAdmin(admin.ModelAdmin):
         "created_at",
         "customer",
     )
+    readonly_fields = ("total_price","total_quantity")
+    fieldsets = (("Total info", {"fields": ("total_price", "total_quantity")},),("Main Info",{"fields":("customer","is_active")}))
+    date_hierarchy = "created_at"
+    @admin.display(description="Status")
+    def active_status(self, obj):
+        if obj.is_active:
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Active</span>'
+            )
+
+        return format_html(
+            '<span style="color: red; font-weight: bold;">Inactive</span>'
+        )
+
 
 admin.site.register(Product)
 admin.site.register(Customer)
--- lab2django/models.py
+++ lab2django/models.py
@@ -8,14 +8,14 @@ from django.utils import timezone
 class Customer(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
-    created_at = models.DateTimeField(default=datetime(2005,1,17))
+    created_at = models.DateTimeField(default=datetime(2005, 1, 17))
 
     def __str__(self):
         return f"Customer {self.name} {self.surname}"
 
 
 class Product(models.Model):
-    created_at = models.DateTimeField(default=datetime(2005,1,17))
+    created_at = models.DateTimeField(default=datetime(2005, 1, 17))
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -23,23 +23,40 @@ class Product(models.Model):
         return f"Product {self.name} {self.price}"
 
 
-
-
 class Cart(models.Model):
-    created_at = models.DateTimeField(default=datetime(2005,1,17))
+    created_at = models.DateTimeField(default=datetime(2005, 1, 17))
     customer = models.ForeignKey(
         Customer,
         on_delete=models.CASCADE,
     )
     products = models.ManyToManyField(Product, through="ProductInCart")
+    is_active = models.BooleanField(default=False)
+
+    @property
+    def total_price(self):
+        total = 0
+        for item in ProductInCart.objects.filter(cart=self).select_related("product"):
+            total += item.product.price * item.quantity
+        return total
+
+    @property
+    def total_quantity(self):
+        total = 0
+        for item in ProductInCart.objects.filter(cart=self).select_related("product"):
+            total += item.quantity
+        return total
+
     def __str__(self):
         return f"Cart id:{self.pk} with customer:{self.customer}"
+
+
 class ProductInCart(models.Model):
-    created_at = models.DateTimeField(default=datetime(2005,1,17))
+    created_at = models.DateTimeField(default=datetime(2005, 1, 17))
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.PositiveBigIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(1000)]
     )
+
     def __str__(self):
         return f"{self.product.name} in count {self.quantity} in cart id:{self.cart.pk}"
